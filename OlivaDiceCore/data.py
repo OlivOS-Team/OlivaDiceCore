6190002b2658f6446ece4f00d4dedd04f468a607
仑质 <benzenpenxil@foxmail.com>
2022-03-17 14:55:05 +0800

    增加描述字段

--- OlivaDiceCore/data.py
+++ OlivaDiceCore/data.py
@@ -21,6 +21,7 @@ import uuid
 
 import OlivOS
 
+OlivaDiceCore_name = 'OlivaDice核心模块'
 OlivaDiceCore_ver = '3.1.10'
 OlivaDiceCore_svn = 1020
 OlivaDiceCore_ver_short = '%s(%s)' % (str(OlivaDiceCore_ver), str(OlivaDiceCore_svn))
