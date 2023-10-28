961143c988c6797c9c5cbed213d3771922eb6f5f
仑质 <benzenpenxil@foxmail.com>
2023-10-28 15:51:37 +0800

    版本推进至3.3.22

--- OlivaDiceCore/data.py
+++ OlivaDiceCore/data.py
@@ -22,8 +22,8 @@ import uuid
 import OlivOS
 
 OlivaDiceCore_name = 'OlivaDice核心模块'
-OlivaDiceCore_ver = '3.3.21'
-OlivaDiceCore_svn = 1071
+OlivaDiceCore_ver = '3.3.22'
+OlivaDiceCore_svn = 1072
 OlivaDiceCore_ver_short = '%s(%s)' % (str(OlivaDiceCore_ver), str(OlivaDiceCore_svn))
 
 exce_path = os.getcwd()
