892fd4da3579e87f8324e675b6d82cb6e667c396
Desom-fu <desom233@outlook.com>
2025-11-21 10:00:30 +0800

    3.4.40

--- OlivaDiceCore/data.py
+++ OlivaDiceCore/data.py
@@ -22,8 +22,8 @@ import uuid
 import OlivOS
 
 OlivaDiceCore_name = 'OlivaDice核心模块'
-OlivaDiceCore_ver = '3.4.39'
-OlivaDiceCore_svn = 1119
+OlivaDiceCore_ver = '3.4.40'
+OlivaDiceCore_svn = 1120
 OlivaDiceCore_ver_short = '%s(%s)' % (str(OlivaDiceCore_ver), str(OlivaDiceCore_svn))
 
 exce_path = os.getcwd()
